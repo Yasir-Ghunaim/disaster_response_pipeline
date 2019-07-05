d4448bfc82fe0ddd8d098af000120227c0c5f994
Yasir Ghunaim <yasirghunaim@gmail.com>
2019-07-05 10:06:44 +0300

    Finished implementing ETL pipeline

--- data/process_data.py
+++ data/process_data.py
@@ -1,16 +1,46 @@
 import sys
-
+import pandas as pd
+import numpy as np
+from sqlalchemy import create_engine
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    """Load messages and categories data from given filepaths and return a combined Dataframe"""
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+    df = messages.merge(categories, left_on='id', right_on='id')
+    return df
 
 
 def clean_data(df):
-    pass
+    """Perform multiple steps of data cleaning such as splitting category column and converting category values to digits """
+
+    # Split categories into separate category columns
+    categories = df["categories"].str.split(";", expand=True)
+
+    # -- Rename new category columns based on the first row
+    firstRow = categories.iloc[0]
+    categories.columns = firstRow.apply(lambda columnName: columnName[:-2])
+
+    # Convert category values to numerical digits of 0 or 1
+    for column in categories:
+      categories[column] = categories[column].apply(lambda element: element[-1])
+      categories[column] = categories[column].astype(int)
+
+    # Replace categories column in df with new category columns
+    df = df.drop(columns=["categories"])
+    df[categories.columns] = categories
+
+    # Remove duplicates
+    df = df.drop_duplicates()
+
+    return df
+
 
 
 def save_data(df, database_filename):
-    pass  
+    """Save the passed dataframe to a database file"""
+    engine = create_engine('sqlite:///' + database_filename)
+    df.to_sql('InsertTableName', engine, index=False)  
 
 
 def main():
